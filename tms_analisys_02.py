03402ec274537036183b846c3dbe05805660905c
Israel CA <israel.cruz.argil@gmail.com>
2014-07-18 09:41:54 -0500

    Algunas mejoras...

--- tms_analisys_02.py
+++ tms_analisys_02.py
@@ -21,48 +21,41 @@
 
 from osv import osv, fields
 import tools
+from tools.translate import _
 
 class tms_analisys_02(osv.Model):
     _name = 'tms.analisys.02'
-    _description = "Order Analisys Tms"
-    _rec_name='activity_name'
+    _description = "MRO Tasks Analisys"
     _auto = False
     _description = 'Order Maintenace Analisys 2'
 
 ########################### Columnas : Atributos #######################################################################
     _columns = {
-        ######## Integer ###########
-        'id':   fields.integer('Activity ID'),
-
-        ######## boolean ###########
-        'external_workshop':   fields.boolean('External'),
-
-        ######## Char ###########
-        'activity_name':   fields.char('Activity'),
-        'order_name':      fields.char('Order'),
-        'order_id':        fields.char('Order ID'),
-        'invoice_name':    fields.char('Invoice Name'),
-        'partner_name':    fields.char('Partner Name'),
-        'unit_name':       fields.char('Unit Name'),
-
-        ######## Float ###########
-        'cost_manpower':        fields.float('Cost Manpower'),
-        'cost_material':        fields.float('Cost Material'),
-        'hours':                fields.float('Hours'),
-
-        ######## Date ###########
-        'date_begin':       fields.datetime('Date Begin'),
-        'date_end':         fields.datetime('Date End'),
-
-        ######## Many2One ###########
-        'unit_id':     fields.many2one('fleet.vehicle','Unit'),
-        'driver_id':   fields.many2one('hr.employee','Driver'),
-        'activity_id': fields.many2one('tms.maintenance.order.activity','Activity'),     
-        'invoice_id':  fields.many2one('account.invoice','Invoice ID'),
-        'partner_id':  fields.many2one('res.partner','Partner ID'),
         
-        ########Related ###########
+        'date_order'    : fields.datetime('Date Order', readonly=True),
+        'order_id'      : fields.many2one('tms.maintenance.order','Order', readonly=True),
+        'name'          : fields.many2one('tms.maintenance.order.activity','Task', readonly=True),
+        'product_id'    : fields.many2one('product.product','Task', readonly=True),
+        'product_category_id' : fields.many2one('product.category','Product Category', readonly=True),
+        'date'          : fields.datetime('Date', readonly=True),
+        'year'           : fields.char('Year', size=4, readonly=True),
+        'day'            : fields.char('Day', size=128, readonly=True),
+        'month'          : fields.selection([('01',_('January')), ('02',_('February')), ('03',_('March')), ('04',_('April')),
+                                        ('05',_('May')), ('06',_('June')), ('07',_('July')), ('08',_('August')), ('09',_('September')),
+                                        ('10',_('October')), ('11',_('November')), ('12',_('December'))], 'Month',readonly=True),        
+        'duration'      : fields.float('Duration', readonly=True),
+        'date_start'    : fields.datetime('Date Start', readonly=True),
+        'date_end'      : fields.datetime('Date End', readonly=True),
+        'external_workshop':   fields.boolean('External', readonly=True),
+        'invoice_id'    : fields.many2one('account.invoice','Invoice Record', readonly=True),
         'supplier_invoice_number': fields.char('Invoice Number', readonly=True),
+        'unit_id'       : fields.many2one('fleet.vehicle','Vehicle', readonly=True),
+        'driver_id'     : fields.many2one('hr.employee','Driver', readonly=True),
+        'supplier_id'   : fields.many2one('res.partner','Supplier', readonly=True, required=False),
+        'spare_parts'   : fields.float('Spare Parts', readonly=True),
+        'manpower'      : fields.float('Manpower', readonly=True),
+        'spare_parts_external'   : fields.float('Spare Parts External', readonly=True),
+        'manpower_external' : fields.float('Manpower External', readonly=True),
     }
     
 ########################### Metodos ####################################################################################
@@ -72,40 +65,25 @@ class tms_analisys_02(osv.Model):
         cr.execute("""
             create or replace view tms_analisys_02 as (
 
-select 
-	(select p.name_template  from product_product as p where p.id = activity.product_id) as activity_name,
-	(activity.id                                                                       ) as activity_id,
-	(activity.id                                                                       ) as id,
-
-    (select u.id            from fleet_vehicle as u where u.id=o.unit_id)                as unit_id,
-    (select u.name          from fleet_vehicle as u where u.id=o.unit_id)                as unit_name,
-
-    (select e.id            from hr_employee   as e where e.id=o.driver_id)              as driver_id,
-    (select e.name_related  from hr_employee   as e where e.id=o.driver_id)              as driver_name,
-
-	activity.date_start_real                                                             as date_begin,
-	activity.date_end_real                                                               as date_end,
-	activity.hours_real                                                                  as hours,
-
-	activity.cost_service                                                                as cost_manpower,
-	activity.parts_cost                                                                  as cost_material,
-	
-	o.name                                                                               as order_name,
-	o.id                                                                                 as order_id,
-
-	activity.external_workshop							                                 as external_workshop,
-	
-	(select i.id from account_invoice as i where i.id = activity.invoice_id)	                    as invoice_id,
-	(select i.name from account_invoice as i where i.id = activity.invoice_id)	                    as invoice_name,
-	(select i.supplier_invoice_number from account_invoice as i where i.id = activity.invoice_id)	as supplier_invoice_number,
-
-	(select p.id from res_partner as p where activity.supplier_id = p.id)                as partner_id,
-	(select p.name from res_partner as p where activity.supplier_id = p.id)              as partner_name
-
-                                                                  
-from tms_maintenance_order_activity as activity, tms_maintenance_order as o
-where activity.state like 'done' and activity.maintenance_order_id = o.id and o.state like 'done'
-order by maintenance_order_id        
+select a.id, o.date as date_order, o.id as order_id, a.id as name, a.product_id, e.categ_id as product_category_id,
+a.date_start as date,
+to_char(date_trunc('day',a.date_start), 'YYYY') as year,
+to_char(date_trunc('day',a.date_start), 'MM') as month,
+to_char(date_trunc('day',a.date_start), 'YYYY-MM-DD') as day,
+a.hours_real as duration, a.date_start_real as date_start, a.date_end_real as date_end, 
+a.external_workshop, a.invoice_id, c.supplier_invoice_number,
+o.unit_id, o.driver_id, a.supplier_id,
+a.parts_cost as spare_parts,
+a.cost_service as manpower,
+a.parts_cost_external as spare_parts_external,
+a.cost_service_external as manpower_external
+from tms_maintenance_order_activity a
+left join tms_maintenance_order as o on o.id=a.maintenance_order_id and o.state='done'
+left join account_invoice as c on c.id=a.invoice_id and c.state <> 'cancel'
+left join product_product d on d.id=a.product_id
+left join product_template e on e.id=d.product_tmpl_id
+where a.state = 'done'
+order by o.date, a.date_start_real
                 
             )
         """)
