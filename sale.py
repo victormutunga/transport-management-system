0d48fdf3b5130f560f844dfe23af3d7a318d279b
german-ponce <german.ponce@hesatecnica.com>
2015-03-27 12:25:12 -0600

    Cambios

--- sale.py
+++ sale.py
@@ -122,17 +122,18 @@ class account_invoice(osv.osv):
         tms_waybill = self.pool.get('tms.waybill')
         waybill_confirmed_ids = tms_waybill.search(cr, uid, [('state','=','confirmed'),('invoice_paid','=',False),('partner_id','=',partner.id)])
         waybill_amount = 0.0
-        cr.execute("""select tw.id from tms_waybill as tw 
-                join account_invoice as aci
-                on aci.id=tw.invoice_id 
-                where tw.state='confirmed' 
-                and aci.state in ('draft','cancel') and tw.id in %s""", 
-                (tuple(waybill_confirmed_ids),))
-        waybill_confirmed_cr = cr.fetchall()
-        waybill_confirmed_ids = []
-        if waybill_confirmed_cr:
-            waybill_confirmed_ids = [x[0] for x in waybill_confirmed_cr]
-                    
+        if waybill_confirmed_ids:
+            cr.execute("""select tw.id from tms_waybill as tw 
+                    join account_invoice as aci
+                    on aci.id=tw.invoice_id 
+                    where tw.state='confirmed' 
+                    and aci.state in ('draft','cancel') and tw.id in %s""", 
+                    (tuple(waybill_confirmed_ids),))
+            waybill_confirmed_cr = cr.fetchall()
+            waybill_confirmed_ids = []
+            if waybill_confirmed_cr:
+                waybill_confirmed_ids = [x[0] for x in waybill_confirmed_cr]
+
         waybill_ids = tms_waybill.search(cr, uid, [('state','=','approved'),('partner_id','=',partner.id)])
         if waybill_confirmed_ids:
             waybill_ids = waybill_ids + waybill_confirmed_ids
@@ -241,16 +242,17 @@ class account_invoice(osv.osv):
                 waybill_confirmed_ids = tms_waybill.search(cr, uid, [('state','=','confirmed'),('invoice_paid','=',False),('partner_id','=',partner_br.id),('id','!=',tuple(waybill_invoice_ids))])
                 # print "############## WAYBILL CONFIRMED SEARCH IDS >>>>>>>> ", waybill_confirmed_ids
                 waybill_amount = 0.0
-                cr.execute("""select tw.id from tms_waybill as tw 
-                join account_invoice as aci
-                on aci.id=tw.invoice_id 
-                where tw.state='confirmed' 
-                and aci.state in ('draft','cancel') and tw.id in %s""", 
-                (tuple(waybill_confirmed_ids),))
-                waybill_confirmed_cr = cr.fetchall()
-                waybill_confirmed_ids = []
-                if waybill_confirmed_cr:
-                    waybill_confirmed_ids = [x[0] for x in waybill_confirmed_cr]
+                if waybill_confirmed_ids:
+                    cr.execute("""select tw.id from tms_waybill as tw 
+                    join account_invoice as aci
+                    on aci.id=tw.invoice_id 
+                    where tw.state='confirmed' 
+                    and aci.state in ('draft','cancel') and tw.id in %s""", 
+                    (tuple(waybill_confirmed_ids),))
+                    waybill_confirmed_cr = cr.fetchall()
+                    waybill_confirmed_ids = []
+                    if waybill_confirmed_cr:
+                        waybill_confirmed_ids = [x[0] for x in waybill_confirmed_cr]
                 # print "################ WAYBILL CONFIRMED IDS FINALLLLL >>>>>>", waybill_confirmed_ids
                 waybill_ids = tms_waybill.search(cr, uid, [('state','=','approved'),('partner_id','=',partner_br.id)])
                 if waybill_confirmed_ids:
