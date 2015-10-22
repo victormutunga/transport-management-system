0680bbad9f1a419e7ee9676161ded7f3db73082e
german-ponce <german.ponce@hesatecnica.com>
2015-10-22 11:33:05 -0500

    Cambios

---
+++ __openerp__.py
@@ -0,0 +1,41 @@
+# -*- encoding: utf-8 -*-
+###########################################################################
+#    Module Writen to OpenERP, Open Source Management Solution
+#
+#    Copyright (c) 2014 Argil Consulting - http://www.argil.mx
+############################################################################
+#    Coded by: German Ponce Dominguez (german.ponce@argil.mx)
+
+{
+    "name": "TMS - Bloqueo de Importes",
+    "version": "1.0",
+    "author": "Argil Consulting",
+    "category": "TMS",
+    "description" : """
+TMS - Bloqueo de Importes
+=========================
+
+Este modulo bloquea  las facturas que provengan de órdenes de mantenimiento y almacén para no modificar los Importes.
+
+1. Este Modulo Crea un Grupo Llamado TMS / Manager Contable, este grupo permite editar un CheckBox llamado Bloquear Importes,
+este campo es el encargado de no permitir editar los Importes, si se desactiva entonces podras realizarlo.
+
+2. Por Defecto en este Grupo Solo se encuentra el Administrador.
+
+    """,
+    "website": "http://www.argil.mx/",
+    "license": "AGPL-3",
+    "depends": [
+            "account",
+            "tms",
+            "tms_maintenance",
+                ],
+    "demo": [],
+    "data": [
+            "tms_view.xml",
+    ],
+    "installable": True,
+    "active": False,
+}
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
