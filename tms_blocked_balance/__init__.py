23dacb326b0705aec40ff77e1a6c8762b0e90b57
SaraiOsorio <sarai_anai@hotmail.com>
2016-03-03 17:59:58 -0600

    [FIX]-changes in tms_blocked_balance

--- tms_blocked_balance/__init__.py
+++ tms_blocked_balance/__init__.py
@@ -6,4 +6,7 @@
 ############################################################################
 #    Coded by: German Ponce Dominguez (german.ponce@argil.mx)
 
-import tms
+import account_invoice
+import tms_maintenance_order_activity_invoice
+import tms_waybill_invoice
+import tms_waybill_supplier_invoice
