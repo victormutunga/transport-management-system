2e72c4137c12d565efd4d8a46f1b7d1eed485261
Jesus Alan Ramos Rodriguez <alan.ramos@jarsa.com.mx>
2016-03-03 18:51:35 -0600

    [FIX][tms_maintenance] fix init

---
+++ tms_maintenance/models/__init__.py
@@ -0,0 +1,42 @@
+# -*- encoding: utf-8 -*-
+##############################################################################
+#    
+#    OpenERP, Open Source Management Solution
+#    Copyright (C) 2004-2009 Tiny SPRL (<http://tiny.be>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.     
+#
+##############################################################################
+
+from . import fleet_vehicle
+from . import fleet_vehicle_mro_program
+from . import fleet_vehicle_mro_program_reschedule
+from . import fleet_vehicle_mro_program_reschedule_line
+from . import product_product
+from . import sale_shop
+from . import stock_move
+from . import stock_picking
+from . import tms_activity_control_time
+from . import tms_analisys_01
+from . import tms_analisys_02
+from . import tms_analisys_03
+from . import tms_analisys_04
+from . import tms_analisys_05 
+from . import tms_maintenance_driver_report
+from . import tms_maintenance_order
+from . import tms_maintenance_order_activity
+from . import tms_maintenance_order_activity_invoice
+from . import tms_product_line
+from . import tms_time
+from . import tms_travel
