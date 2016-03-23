dd457ef1a2fa902979eb93024b04c9551ee62bde
Jesus Alan Ramos Rodriguez <alan.ramos@jarsa.com.mx>
2016-03-23 15:06:49 +0000

    [ADD][tms] Settings menu to install modules from a functional view

--- tms/models/__init__.py
+++ tms/models/__init__.py
@@ -1,59 +1,60 @@
 # -*- encoding: utf-8 -*-
 
-# import account_account
-# import account_invoice
-# import account_invoice_line
-# import account_journal
-# import account_move_line
-# import account_tax
-# import fleet_vehicle
-# import fleet_vehicle_odometer
-# import fleet_vehicle_odometer_device
-# import hr_employee
-# import hr_job
-# import ir_sequence
-# import product_category
-# import product_product
-# import sale_shop
-# import tms_advance
-# import tms_advance_invoice
-# import tms_advance_payment
-# import tms_event
-# import tms_event_action
-# import tms_event_category
-# import tms_expense
-# import tms_expense_cancel
-# import tms_expense_invoice
-# import tms_expense_line
-# import tms_expense_loan
-# import tms_expense_payment
-# import tms_factor
-# import tms_factor_special
-# import tms_fuelvoucher
-# import tms_fuelvoucher_invoice
-# import tms_operation
-# import tms_place
-# import tms_route
-# import tms_route_fuelefficiency
-# import tms_route_place
-# import tms_route_tollstation
-# import tms_route_tollstation_costperaxis
-# import tms_sale_shop_fuel_supplier_seq
-# import tms_travel
-# import tms_unit_active_history
-# import tms_unit_category
-# import tms_unit_expiry
-# import tms_unit_extradata
-# import tms_unit_kit
-# import tms_unit_photo
-# import tms_unit_red_tape
-# import tms_waybill
-# import tms_waybill_cancel
-# import tms_waybill_category
-# import tms_waybill_extradata
-# import tms_waybill_invoice
-# import tms_waybill_line
-# import tms_waybill_shipped_grouped
-# import tms_waybill_shipped_product
-# import tms_waybill_supplier_invoice
-# import tms_waybill_taxes
+# from . import account_account
+# from . import account_invoice
+# from . import account_invoice_line
+# from . import account_journal
+# from . import account_move_line
+# from . import account_tax
+# from . import fleet_vehicle
+# from . import fleet_vehicle_odometer
+# from . import fleet_vehicle_odometer_device
+# from . import hr_employee
+# from . import hr_job
+# from . import ir_sequence
+# from . import product_category
+# from . import product_product
+# from . import sale_shop
+# from . import tms_advance
+# from . import tms_advance_invoice
+# from . import tms_advance_payment
+from . import tms_config_settings
+# from . import tms_event
+# from . import tms_event_action
+# from . import tms_event_category
+# from . import tms_expense
+# from . import tms_expense_cancel
+# from . import tms_expense_invoice
+# from . import tms_expense_line
+# from . import tms_expense_loan
+# from . import tms_expense_payment
+# from . import tms_factor
+# from . import tms_factor_special
+# from . import tms_fuelvoucher
+# from . import tms_fuelvoucher_invoice
+# from . import tms_operation
+# from . import tms_place
+# from . import tms_route
+# from . import tms_route_fuelefficiency
+# from . import tms_route_place
+# from . import tms_route_tollstation
+# from . import tms_route_tollstation_costperaxis
+# from . import tms_sale_shop_fuel_supplier_seq
+# from . import tms_travel
+# from . import tms_unit_active_history
+# from . import tms_unit_category
+# from . import tms_unit_expiry
+# from . import tms_unit_extradata
+# from . import tms_unit_kit
+# from . import tms_unit_photo
+# from . import tms_unit_red_tape
+# from . import tms_waybill
+# from . import tms_waybill_cancel
+# from . import tms_waybill_category
+# from . import tms_waybill_extradata
+# from . import tms_waybill_invoice
+# from . import tms_waybill_line
+# from . import tms_waybill_shipped_grouped
+# from . import tms_waybill_shipped_product
+# from . import tms_waybill_supplier_invoice
+# from . import tms_waybill_taxes
