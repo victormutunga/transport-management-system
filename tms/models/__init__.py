d48e1c92c943f3d59469cad298e23ced0c55dff3
SaraiOsorio <sarai_anai@hotmail.com>
2016-04-06 12:39:27 -0500

    [tms] init of TMS Base

--- tms/models/__init__.py
+++ tms/models/__init__.py
@@ -1,60 +1,21 @@
 # -*- encoding: utf-8 -*-
 
-# from . import account_account
-# from . import account_invoice
-# from . import account_invoice_line
 # from . import account_journal
-# from . import account_move_line
-# from . import account_tax
-from . import fleet_vehicle
-from . import fleet_vehicle_odometer
-# from . import fleet_vehicle_odometer_device
-from . import hr_employee
-# from . import hr_job
-# from . import ir_sequence
+# from . import fleet_vehicle
+# from . import fleet_vehicle_odometer
+# from . import hr_employee
 # from . import product_category
 # from . import product_product
-from . import res_partner
-from . import tms_advance
-# from . import tms_advance_invoice
-# from . import tms_advance_payment
-from . import tms_config_settings
-# from . import tms_event
-# from . import tms_event_action
-# from . import tms_event_category
-from . import tms_expense
-# from . import tms_expense_cancel
-# from . import tms_expense_invoice
-from . import tms_expense_line
-# from . import tms_expense_loan
-# from . import tms_expense_payment
-from . import tms_factor
-from . import tms_factor_special
-from . import tms_fuelvoucher
-# from . import tms_fuelvoucher_invoice
-# from . import tms_operation
-from . import tms_place
-from . import tms_route
-from . import tms_route_fuelefficiency
-from . import tms_route_place
-from . import tms_route_tollstation
-from . import tms_route_tollstation_costperaxis
-# from . import tms_sale_shop_fuel_supplier_seq
-from . import tms_travel
-from . import tms_unit_active_history
-from . import tms_unit_category
-from . import tms_unit_expiry
-from . import tms_unit_extradata
-from . import tms_unit_kit
-from . import tms_unit_photo
-from . import tms_unit_red_tape
-from . import tms_waybill
-# from . import tms_waybill_cancel
-from . import tms_waybill_category
-from . import tms_waybill_extradata
-# from . import tms_waybill_invoice
-from . import tms_waybill_line
-# from . import tms_waybill_shipped_grouped
-from . import tms_waybill_shipped_product
-# from . import tms_waybill_supplier_invoice
-from . import tms_waybill_taxes
+# from . import tms_advance
+# from . import tms_config_settings
+# from . import tms_expense
+# from . import tms_expense_line
+# from . import tms_factor
+# from . import tms_place
+# from . import tms_route
+# from . import tms_travel
+# from . import tms_unit_kit
+# from . import tms_waybill
+# from . import tms_waybill_line
+# from . import tms_waybill_shipped_product
+# from . import tms_waybill_taxes
