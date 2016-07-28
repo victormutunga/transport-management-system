2a288e5a1161c112e7b96d9e70ee14b6bb634c4b
Sarai Osorio <sarai.osorio@jarsa.com.mx>
2016-07-28 10:40:00 -0500

    [add][tms_waybill_line]-add methods (#69)

--- tms/models/tms_waybill_line.py
+++ tms/models/tms_waybill_line.py
@@ -3,7 +3,7 @@
 # © <2016> <Jarsa Sistemas, S.A. de C.V.>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from openerp import fields, models
+from openerp import _, api, fields, models
 
 
 class TmsWaybillLine(models.Model):
@@ -62,3 +62,42 @@ class TmsWaybillLine(models.Model):
         'res.partner',
         string='Customer')
     control = fields.Boolean()
+
+    @api.depends('price_unit', 'product_uom_qty')
+    @api.multi
+    def _amount_line(self):
+        tax_obj = self.env['account.tax']
+        for line in self:
+            price = line.price_unit * (1 - (line.discount or 0.0) / 100.0)
+            taxes = tax_obj.compute_all(
+                line.tax_id, price, line.product_uom_qty,
+                line.product_id, line.waybill_id.partner_id)
+            currency = line.waybill_id.currency_id
+            line.price_total = self.currency_id.round(
+                currency, taxes['total_included'])
+            line.tax_amount = self.currency_id.round(
+                currency, taxes['total_included']) - self.currency_id.round(
+                currency, taxes['total'])
+            line.price_subtotal = line.price_unit * line.product_uom_qty
+            line.price_discount = line.price_subtotal * (
+                (line.discount or 0.0) / 100.0)
+
+    @api.onchange('product_uom_qty', 'price_unit',
+                  'discount', 'product_id', 'partner_id')
+    def on_change_amount(self):
+        tax_factor = 0.00
+        if tax_factor == 0.00:
+            raise Warning(
+                _('No taxes defined in product !'),
+                _('You have to add taxes for this product. Para Mexico: '
+                  'Tiene que agregar el IVA que corresponda y el IEPS con '
+                  'factor 0.0.'))
+
+        self.price_amount = self.price_unit * self.product_uom_qty
+        self.price_discount = (
+            (self.price_unit * self.product_uom_qty) * (self.discount / 100.0))
+        self.price_subtotal = self.price_amount - self.price_discount
+        self.tax_amount = (
+            (self.price_amount - self.price_discount) * tax_factor)
+        self.price_total = (
+            (self.price_amount - self.price_discount) * (1.0 + tax_factor))
