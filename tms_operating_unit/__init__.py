37acdbb9d4afbfb1aba9a7178f3f6e540d68eff6
Alan Ramos <alan.ramos@jarsa.com.mx>
2016-09-03 12:55:53 -0500

    [REM] Unused modules

--- tms_operating_unit/__init__.py
+++
@@ -1,5 +0,0 @@
-# -*- coding: utf-8 -*-
-# © 2016 Jarsa Sistemas, S.A. de C.V.
-# - Jesús Alan Ramos Rodríguez
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
-from . import models
