c3660ee679a060be0c9e8722149e83ff54bb8684
Alan Ramos <alan.ramos@jarsa.com.mx>
2017-01-04 17:34:11 -0800

    [REF] amount_driver in tms.factor

--- tms/models/tms_advance.py
+++ tms/models/tms_advance.py
@@ -47,7 +47,7 @@ class TmsAdvance(models.Model):
         readonly=True)
     paid = fields.Boolean(
         compute='_compute_paid',
-        readonly=True, store=True)
+        readonly=True)
     payment_move_id = fields.Many2one(
         'account.move',
         string="Payment Entry",
--- tms/models/tms_factor.py
+++ tms/models/tms_factor.py
@@ -32,7 +32,8 @@ class TmsFactor(models.Model):
         ('qty', 'Quantity'),
         ('volume', 'Volume'),
         ('percent', 'Income Percent'),
-        ('percent_driver', 'Income Percent per Driver')], string='Factor Type',
+        ('percent_driver', 'Income Percent per Driver'),
+        ('amount_driver', 'Amount Percent per Driver')], string='Factor Type',
         required=True,
         help='For next options you have to type Ranges or Fixed Amount\n - '
              'Distance Route (Km/mi)\n - Distance Real (Km/Mi)\n - Weight\n'
@@ -92,6 +93,15 @@ class TmsFactor(models.Model):
                     raise ValidationError(
                         _('Invalid parameter you can use this factor only with'
                           ' drivers'))
+            elif rec.factor_type == 'amount_driver':
+                if employee:
+                    if employee.income_percentage == 0.0:
+                        raise ValidationError(
+                            _('The employee must have a income percentage '
+                              'value'))
+                    else:
+                        amount += rec.fixed_amount * (
+                            employee.income_percentage / 100)
             else:
                 for key, value in factor_list.items():
                     if rec.factor_type == key:
