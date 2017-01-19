38347f7d758b52a6231b0402abfa8e446c5d8bdd
lmgr0312 <lmiguel.gruiz@gmail.com>
2017-01-19 13:37:40 -0600

    [REF][Letter of Instructions Report]

--- tms/models/account_invoice.py
+++ tms/models/account_invoice.py
@@ -3,7 +3,7 @@
 # Copyright 2016, Jarsa Sistemas, S.A. de C.V.
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from openerp import fields, models
+from openerp import api, fields, models
 
 
 class AccountInvoice(models.Model):
