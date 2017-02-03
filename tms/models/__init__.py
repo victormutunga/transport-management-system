4e9a335fc64737d4a0e69b1a428b4ee2dbf61e68
ougc27 <oscar_garzacordova@hotmail.com>
2017-02-03 11:02:21 -0600

    [FIX][tms] - Paid Field in Expenses / Advances.

--- tms/models/__init__.py
+++ tms/models/__init__.py
@@ -4,6 +4,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 from . import account_invoice
+from . import account_move
 from . import fleet_vehicle
 from . import fleet_vehicle_engine
 from . import fleet_vehicle_log_fuel
