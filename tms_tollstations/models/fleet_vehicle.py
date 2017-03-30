6f9ff9cff261d7254bf3f1b297aa785e77ee0bf6
Chandresh Thakkar <chandresh.thakkar.serpentcs@gmail.com>
2017-03-30 20:20:04 +0530

    [ADD] Migrated tms_tollstations module in version 10.

---
+++ tms_tollstations/models/fleet_vehicle.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+# Copyright 2016, Jarsa Sistemas, S.A. de C.V.
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from odoo import api, fields, models
+
+
+class FleetVehicle(models.Model):
+    _inherit = 'fleet.vehicle'
+
+    tollstation_tag = fields.Char()
---
+++ tms_tollstations/models/tms_toll_data.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# Copyright 2012, Israel Cruz Argil, Argil Consulting
+# Copyright 2016, Jarsa Sistemas, S.A. de C.V.
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from odoo import fields, models
+
+
+class TmsTollData(models.Model):
+    _name = 'tms.toll.data'
+
+    date = fields.Datetime()
+    name = fields.Char()
+    num_tag = fields.Char(string='Tag number')
+    economic_number = fields.Char()
+    import_rate = fields.Float()
+    expense_line_id = fields.Many2one(
+        'tms.expense.line', string='Expense line')
+    state = fields.Selection([
+        ('open', 'Open'),
+        ('closed', 'Closed')],
+        default='open')
