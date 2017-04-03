f9cd8270142f3009d81d1f5a8e051332fe4a37dd
SaraiOsorio <sarai.osorio@jarsa.com.mx>
2017-04-03 17:30:31 -0500

    [ADD][tms_expense_retention]- add new module for retention

---
+++ tms_expense_retention/models/__init__.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+# Copyright 2016, Jarsa Sistemas, S.A. de C.V.
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from . import tms_retention
+from . import hr_employee
+from . import product_template
+from . import tms_expense
---
+++ tms_expense_retention/models/tms_expense.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+# Copyright 2016, Jarsa Sistemas, S.A. de C.V.
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from openerp import _, api, models
+from openerp.exceptions import ValidationError
+
+
+class TmsExpense(models.Model):
+    _inherit = 'tms.expense'
+
+    @api.multi
+    def get_isr(self):
+        for rec in self:
+            value = 0.0
+            obj_retention = self.env['tms.retention']
+            product = self.env['product.product'].search(
+                [('name', '=', 'ISR'), ('apply_for_retention', '=', True)],
+                limit=1)
+            if not product:
+                raise ValidationError(_('You must have a product called isr'))
+            retention = obj_retention.search([
+                ('product_id', '=', product.id)])
+            days = int(rec.travel_days.split('D')[0])
+            if retention.employee_ids:
+                if rec.employee_id in retention.employee_ids:
+                    value = retention.factor * days
+            else:
+                value = retention.factor * days
+            if value > 0.0:
+                rec.expense_line_ids.create({
+                    'name': "ISR",
+                    'expense_id': rec.id,
+                    'line_type': "salary_retention",
+                    'product_qty': 1.0,
+                    'product_uom_id': product.uom_id.id,
+                    'product_id': product.id,
+                    'unit_price': value,
+                    'control': True
+                })
+
+    @api.multi
+    def get_travel_info(self):
+        for rec in self:
+            super(TmsExpense, self).get_travel_info()
+            rec.get_isr()
