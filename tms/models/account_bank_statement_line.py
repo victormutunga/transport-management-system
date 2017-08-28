2e37bcd0a3294ac4c2c913bd2ba68c276cf58d83
Hector-Camacho <hector.camacho.f@gmail.com>
2017-08-28 18:41:35 -0500

    [FIX] Document import account bank statement

--- tms/models/account_bank_statement_line.py
+++ tms/models/account_bank_statement_line.py
@@ -26,6 +26,7 @@ class AccountBankStatementLine(models.Model):
                     home_address.tms_expense_negative_account_id.id)
                 accounts = [account_employee,
                             line.partner_id.property_account_payable_id.id]
+                # Line.full_reconcile_id.id sometimes is False
                 payment_line = (
                     line.full_reconcile_id.
                     reconciled_line_ids.search([
@@ -50,5 +51,4 @@ class AccountBankStatementLine(models.Model):
                     for loan in loans:
                         loan.paid = True
                         loan.payment_move_id = res.id
-
             return res
