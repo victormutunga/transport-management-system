18725593469aa8de2b862f9d3e80ce914f050273
SaraiOsorio <sarai.osorio@jarsa.com.mx>
2017-11-23 17:35:25 -0600

    [ADD][tms]- add new page in waybill

--- tms/models/tms_waybill.py
+++ tms/models/tms_waybill.py
@@ -181,6 +181,16 @@ class TmsWaybill(models.Model):
         'waybill_id',
         string="Customs")
 
+    expense_ids = fields.Many2many(
+        'tms.expense', compute='_get_expenses', string="Expenses")
+
+    @api.depends('travel_ids')
+    def _get_expenses(self):
+        for rec in self:
+            rec.expense_ids = False
+            for travel in rec.travel_ids:
+                rec.expense_ids += travel.expense_id
+
     @api.model
     def create(self, values):
         waybill = super(TmsWaybill, self).create(values)
